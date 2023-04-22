e1f4b66e8142025145ec9e516152598e66427c67
Prateek Thakur <prateekthakur272@gmail.com>
2023-04-22 21:03:11 +0530

    initialise firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:bunkmate/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
 
