71ff254bf605405c9d3643ea4a94f5e4849f9e91
Prateek Thakur <prateekthakur272@gmail.com>
2023-04-29 21:15:38 +0530

    changes item view

--- lib/main.dart
+++ lib/main.dart
@@ -30,26 +30,50 @@ class App extends StatelessWidget {
                   borderRadius: BorderRadius.all(Radius.circular(12)),
                 )),
           ),
+          outlinedButtonTheme: OutlinedButtonThemeData(
+            style: OutlinedButton.styleFrom(
+                side: BorderSide(color: Colors.green.shade300),
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8)))),
+          ),
+          cardTheme: const CardTheme(
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                  side: BorderSide(color: Colors.grey)
+                  )),
         ),
         darkTheme: ThemeData(
-          primarySwatch: lightSkyBlue,
-          canvasColor: Colors.black87,
-          textTheme: const TextTheme(
-            bodySmall: TextStyle(color: Colors.white),
-            bodyMedium: TextStyle(color: Colors.white),
-            bodyLarge: TextStyle(color: Colors.white),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.black,
-                padding: const EdgeInsets.all(16),
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
-                )),
-          ),
-          cardColor: Colors.black,
-        ),
+            primarySwatch: lightSkyBlue,
+            canvasColor: Colors.black87,
+            textTheme: const TextTheme(
+              bodySmall: TextStyle(color: Colors.white),
+              bodyMedium: TextStyle(color: Colors.white),
+              bodyLarge: TextStyle(color: Colors.white),
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                  padding: const EdgeInsets.all(16),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                  )),
+            ),
+            cardTheme: const CardTheme(
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    side: BorderSide(
+                      color: Colors.white,
+                    ))),
+            outlinedButtonTheme: OutlinedButtonThemeData(
+              style: OutlinedButton.styleFrom(
+                  foregroundColor: Colors.green.shade300,
+                  side: BorderSide(color: Colors.green.shade300),
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(18)))),
+            )),
         home: const LoginHomeController());
   }
 }
--- lib/widgets/item.dart
+++ lib/widgets/item.dart
@@ -1,7 +1,6 @@
 import 'package:bunkmate/repository/database.dart';
 import 'package:bunkmate/screens/selected_item.dart';
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class Item extends StatelessWidget {
   final MapEntry entry;
@@ -10,82 +9,89 @@ class Item extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double percentage = Database.percentage(entry.value['total'],entry.value['attended']);
-    return InkWell(
-      onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context)=>SelectedItem(title: entry.key, record: entry.value)));
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              entry.key,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            LinearPercentIndicator(
-                  lineHeight: 28,
-                  animation: true,
-                  percent: percentage,
-                  center: Text(
-                    "${(percentage*100).round()}%",
-                    style:
-                        const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  barRadius: const Radius.circular(8),
-                  progressColor: percentage>=.75?Colors.green.shade300:Colors.red.shade200,
-                ),
-            const SizedBox(
-              height: 8,
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Row(
+    var message = '';
+    if(percentage>.9){
+      message = 'You are doing great, keep it up';
+    }
+    else if(percentage>.75){
+      message = 'You have mantained good attendance';
+    }else if (percentage >= .6){
+      message = 'Your attandance is low, attend regular classes to manatain';
+    }else{
+      message = 'Your attendance is too poor, please attend regular classes';
+    }
+    return Padding(
+      padding: const EdgeInsets.only(top: 16,left: 16,right: 16),
+      child: Card(
+        child: InkWell(
+          borderRadius: const BorderRadius.all(Radius.circular(16)),
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>SelectedItem(title: entry.key)));
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Expanded(
-                  child: OutlinedButton(
-                      onPressed: () async {
-    
-                      },
-                      style: OutlinedButton.styleFrom(
-                        foregroundColor: Colors.green.shade300,
-                        side: BorderSide(color: Colors.green.shade300)
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(Icons.done),
-                          Text('Attanded')
-                        ],
-                      ),
-                  ),
+                Text(
+                  entry.key,
+                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
-                const SizedBox(width:16,),
-                Expanded(
-                  child: OutlinedButton(
-                      onPressed: () {
-    
-                      },
-                      style: OutlinedButton.styleFrom(
-                        foregroundColor: Colors.red.shade300,
-                        side: BorderSide(color: Colors.red.shade300)
+                const SizedBox(
+                  height: 8,
+                ),
+                Text('Overall attendance is ${(percentage*100).round()}%'),
+                Text(
+                  message,
+                  style: const TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: OutlinedButton(
+                          onPressed: () async {
+        
+                          },
+                          style: OutlinedButton.styleFrom(
+                            foregroundColor: Colors.green.shade300,
+                            side: BorderSide(color: Colors.green.shade300)
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Icon(Icons.done),
+                              Text('Attanded')
+                            ],
+                          ),
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(Icons.close),
-                          Text('Missed')
-                        ],
+                    ),
+                    const SizedBox(width:16,),
+                    Expanded(
+                      child: OutlinedButton(
+                          onPressed: () {
+        
+                          },
+                          style: OutlinedButton.styleFrom(
+                            foregroundColor: Colors.red.shade300,
+                            side: BorderSide(color: Colors.red.shade300)
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Icon(Icons.close),
+                              Text('Missed')
+                            ],
+                          ),
                       ),
-                  ),
+                    )
+                  ],
                 )
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
